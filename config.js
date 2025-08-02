b0f7871d1e2b2873e77299bf855453384e65933a
Mr Zasa V3 <zathsaraoppo@gmail.com>
2025-08-02 14:12:01 +0530

    PC

--- config.js
+++ config.js
@@ -5,5 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "VISPER-MD&y5R0UTqR#tvqC-uORv1KeA91JD6udYheLhkXaYutaFu0HeVf9JT8",
+ALIVE_IMG: process.env.ALIVE_IMG || " ( IMAGE URL ) ",
+ALIVE_MSG: process.env.ALIVE_MSG || "TESTING ALIVE MESSAGE. . .",
+AUTOREAD_STATUS: process.env.AUTOREAD_STATUS,
 };

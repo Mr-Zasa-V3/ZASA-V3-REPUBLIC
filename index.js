e4e74b8d2c6c49a8225f43f4ed40ed2aabcb892c
Mr Zasa V3 <zathsaraoppo@gmail.com>
2025-08-03 16:41:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94768949545']
+const ownerNumber = ['94783795020']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
